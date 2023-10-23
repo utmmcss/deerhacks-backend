933bdaeb2f3c4e7d1037ac3299e1f36e15cb83c2
Multivalence <mulwanishiva@gmail.com>
2023-10-23 11:01:15 -0400

    Resolved issue that caused code to crash in /user-update

--- models/userModel.go
+++ models/userModel.go
@@ -7,18 +7,19 @@ import (
 )
 
 type Status string
+
 const (
-	Pending         Status = "pending"          // Pending Email Verification
-	Registering     Status = "registering"      // Email Verified, Registering for DeerHacks
-	Applied         Status = "applied"          // Application Submitted
-	Selected        Status = "selected"         // Selected to Attend DeerHacks, Pending Confirmation
-	Accepted        Status = "accepted"         // Accepted to Attend DeerHacks
-	Rejected        Status = "rejected"         // Application Rejected
-	Attended        Status = "attended"         // Signed in at DeerHacks
-	
-	Admin           Status = "admin"            // DeerHacks Tech Organizers
-	Moderator       Status = "moderator"        // DeerHacks Moderators
-	Volunteer       Status = "volunteer"        // DeerHacks Volunteers
+	Pending     Status = "pending"     // Pending Email Verification
+	Registering Status = "registering" // Email Verified, Registering for DeerHacks
+	Applied     Status = "applied"     // Application Submitted
+	Selected    Status = "selected"    // Selected to Attend DeerHacks, Pending Confirmation
+	Accepted    Status = "accepted"    // Accepted to Attend DeerHacks
+	Rejected    Status = "rejected"    // Application Rejected
+	Attended    Status = "attended"    // Signed in at DeerHacks
+
+	Admin     Status = "admin"     // DeerHacks Tech Organizers
+	Moderator Status = "moderator" // DeerHacks Moderators
+	Volunteer Status = "volunteer" // DeerHacks Volunteers
 )
 
 type User struct {
@@ -27,7 +28,7 @@ type User struct {
 	Avatar         string
 	Name           string `gorm:"size:128"`
 	Email          string `gorm:"unique;size:128"`
-	Status         string `gorm:"default:pending"`
+	Status         Status `gorm:"default:pending"`
 	QRCode         string `gorm:"unique"`
 	InternalStatus string
 	InternalNotes  string
