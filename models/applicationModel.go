ffbc1d26a3eef5bd68d2df8ea0a6be782ec3f894
Hani Asim <haniasim@gmail.com>
2023-10-28 22:35:22 -0400

    added `ShirtSize` type & linting

--- models/applicationModel.go
+++ models/applicationModel.go
@@ -4,42 +4,52 @@ import (
 	"gorm.io/gorm"
 )
 
+type ShirtSize string
+
+const (
+	ExtraSmall  ShirtSize = "xs"
+	Small       ShirtSize = "s"
+	Medium      ShirtSize = "m"
+	Large       ShirtSize = "l"
+	ExtraLarge  ShirtSize = "xl"
+)
+
 type Application struct {
 	gorm.Model
-	DiscordId             string `gorm:"unique;size:128"`
-	IsDraft               bool   `gorm:"default:true"`
-	PhoneNumber           string `gorm:"unique;size:128"`
+	DiscordId             string   `gorm:"unique;size:128"`
+	IsDraft               bool     `gorm:"default:true"`
+	PhoneNumber           string   `gorm:"unique;size:128"`
 	IsSubscribed          bool
-	Age                   int    `gorm:"default:NULL"`
-	Gender                string `gorm:"size:128"`
-	Pronoun               string `gorm:"size:128"`
-	Ethnicity             string `gorm:"size:128"`
-	Country               string `gorm:"size:128"`
-	City                  string `gorm:"size:128"`
-	Province              string `gorm:"size:128"`
-	EmergencyName         string `gorm:"size:128"`
-	EmergencyNumber       string `gorm:"size:128"`
-	EmergencyRelationship string `gorm:"size:128"`
-	ShirtSize             string `gorm:"size:128"`
+	Age                   int
+	Gender                string   `gorm:"size:128"`
+	Pronoun               string   `gorm:"size:128"`
+	Ethnicity             string   `gorm:"size:128"`
+	Country               string   `gorm:"size:128"`
+	City                  string   `gorm:"size:128"`
+	Province              string   `gorm:"size:128"`
+	EmergencyName         string   `gorm:"size:128"`
+	EmergencyNumber       string   `gorm:"size:128"`
+	EmergencyRelationship string   `gorm:"size:128"`
+	ShirtSize             ShirtSize
 	DietRestriction       []string `gorm:"type:jsonb"`
-	AdditionalInfo        string `gorm:"size:128"`
-	Education             string `gorm:"size:128"`
-	School                string `gorm:"size:128"`
-	Program               string `gorm:"size:128"`
-	ResumeLink            string `gorm:"size:128"`
-	ResumeFilename        string `gorm:"size:128"`
-	ResumeHash            string `gorm:"unique;size:128"`
-	Github                string `gorm:"size:128"`
-	Linkedin              string `gorm:"size:128"`
+	AdditionalInfo        string   `gorm:"size:128"`
+	Education             string   `gorm:"size:128"`
+	School                string   `gorm:"size:128"`
+	Program               string   `gorm:"size:128"`
+	ResumeLink            string   `gorm:"size:128"`
+	ResumeFilename        string   `gorm:"size:128"`
+	ResumeHash            string   `gorm:"unique;size:128"`
+	Github                string   `gorm:"size:128"`
+	Linkedin              string   `gorm:"size:128"`
 	ResumeConsent         bool
-	HackathonExperience   string `gorm:"size:1300"`
+	HackathonExperience   string   `gorm:"size:1300"`
 	DeerhacksExperience   bool
-	TeamPreference        string `gorm:"size:128"`
+	TeamPreference        string   `gorm:"size:128"`
 	Interests             []string `gorm:"type:jsonb"`
-	DeerhacksPitch        string `gorm:"size:1300"`
-	SharedProject         string `gorm:"size:1300"`
-	FutureTech            string `gorm:"size:1300"`
-	DeerhacksReach        string `gorm:"size:1300"`
+	DeerhacksPitch        string   `gorm:"size:1300"`
+	SharedProject         string   `gorm:"size:1300"`
+	FutureTech            string   `gorm:"size:1300"`
+	DeerhacksReach        string   `gorm:"size:1300"`
 	MlhCodeAgreement      bool
 	MlhSubscribe          bool
 	MlhAuthorize          bool
