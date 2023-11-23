42c68fc3e0c59c36f109d96a1b6107a8e6995af7
Hani Asim <haniasim@gmail.com>
2023-11-23 02:50:04 -0500

    implement UpdateApplication w/ new helper

--- controllers/applicationController.go
+++ controllers/applicationController.go
@@ -2,7 +2,12 @@ package controllers
 
 import (
 	//"encoding/json"
+	"encoding/json"
+	"io"
 	"net/http"
+	"reflect"
+
+	"github.com/jinzhu/copier"
 
 	"github.com/gin-gonic/gin"
 	"github.com/utmmcss/deerhacks-backend/helpers"
@@ -49,4 +54,87 @@ func GetApplicaton(c *gin.Context) {
 
 	c.JSON(http.StatusOK, applicationResponse)
 
+}
+func UpdateApplication(c *gin.Context) {
+
+	userObj, _ := c.Get("user")
+	user := userObj.(models.User)
+
+	userDiscordId := user.DiscordId
+
+	var application models.Application
+	initializers.DB.First(&application, "discord_id = ?", userDiscordId)
+
+	// If application does not exist, return error
+	if application.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Application not found",
+		})
+		return
+	}
+
+	// If user is not registering or their application is not a draft, return error
+	// Admins can update applications at any time
+	if (user.Status != models.Registering || !application.IsDraft) && user.Status != models.Admin {
+		c.JSON(http.StatusForbidden, gin.H{
+			"error": "User is not allowed to update application at this time",
+		})
+		return
+	}
+
+	// Get the request body
+	bodyObj, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid Request Body",
+		})
+		return
+	}
+	defer c.Request.Body.Close()
+
+	// Defaults to current application values
+	bodyData := helpers.ToApplicationResponse(application)
+
+	if json.Unmarshal(bodyObj, &bodyData) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid Request Body",
+		})
+		return
+	}
+
+	// If application is unchanged, return
+	if reflect.DeepEqual(bodyData, helpers.ToApplicationResponse(application)) {
+		c.Status(http.StatusOK)
+		return
+	}
+
+	// Update the application object with the new information (if applicable)
+	application.IsDraft = bodyData.IsDraft
+	if copier.Copy(&application, &(bodyData.Application)) != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to update application",
+		})
+		return
+	}
+
+	// Validate application if updated as non-draft
+	if !application.IsDraft {
+		valid, err := helpers.ValidateApplication(application)
+		if !valid {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+	}
+
+	// Save the updated application object to the database
+	if initializers.DB.Save(&application).Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to update application",
+		})
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
---
+++ helpers/validateApplication.go
@@ -0,0 +1,92 @@
+package helpers
+
+import (
+	"github.com/jackc/pgtype"
+	"github.com/utmmcss/deerhacks-backend/models"
+)
+
+func ValidateApplication(application models.Application) (bool, string) {
+	if application.PhoneNumber == "" || len(application.PhoneNumber) < 10 {
+		return false, "Phone number is required"
+	}
+	if application.Age < 18 {
+		return false, "You must be at least 18 years old to apply"
+	}
+	if application.Gender == "" {
+		return false, "Gender is required"
+	}
+	if application.Pronoun == "" {
+		return false, "Pronoun is required"
+	}
+	if application.Ethnicity == "" {
+		return false, "Ethnicity is required"
+	}
+	if application.Country == "" {
+		return false, "Country is required"
+	}
+	if application.City == "" {
+		return false, "City is required"
+	}
+	if application.Province == "" {
+		return false, "Province is required"
+	}
+	if application.EmergencyName == "" {
+		return false, "Emergency contact name is required"
+	}
+	if application.EmergencyNumber == "" || len(application.EmergencyNumber) < 10 {
+		return false, "Emergency contact number is required"
+	}
+	if application.EmergencyRelationship == "" {
+		return false, "Emergency contact relationship is required"
+	}
+	if application.ShirtSize == "" {
+		return false, "Shirt size is required"
+	}
+	if application.DietRestriction.Status != pgtype.Present {
+		return false, "Diet restrictions field missing"
+	}
+	if application.Education == "" {
+		return false, "Education level is required"
+	}
+	if application.School == "" {
+		return false, "School is required"
+	}
+	if application.Program == "" {
+		return false, "Program is required"
+	}
+	// TODO: Uncomment when resume upload is implemented
+	// if application.ResumeLink == "" {
+	// 	return false, "Resume link is required"
+	// }
+	// if application.ResumeFilename == "" {
+	// 	return false, "Resume filename is required"
+	// }
+	// if application.ResumeHash == nil {
+	// 	return false, "Resume hash is required"
+	// }
+	if application.HackathonExperience == "" {
+		return false, "Hackathon experience is required"
+	}
+	if application.TeamPreference == "" {
+		return false, "Team preference is required"
+	}
+	if application.Interests.Status != pgtype.Present {
+		return false, "Interests field missing"
+	}
+	if application.DeerhacksPitch == "" {
+		return false, "Deerhacks pitch is required"
+	}
+	if application.SharedProject == "" {
+		return false, "Shared project is required"
+	}
+	if application.FutureTech == "" {
+		return false, "Future tech is required"
+	}
+	if application.DeerhacksReach == "" {
+		return false, "Deerhacks reach is required"
+	}
+	if !application.MlhCodeAgreement {
+		return false, "MLH code agreement is required"
+	}
+	return true, ""
+}
